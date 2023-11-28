3c5cffe817d31e2dd2daf7828fd29123cf9487e4
Chen Yue <yuechen049@gmail.com>
2023-11-28 14:25:22 -0500

    initial commit

---
+++ src/project1/CashierCalculator.java
@@ -0,0 +1,7 @@
+package project1;
+
+public class CashierCalculator {
+    public static void main(String[] args) {
+        ;
+    }
+}
---
+++ src/project2/PatternPrinter.java
@@ -0,0 +1,7 @@
+package project2;
+
+public class PatternPrinter {
+    public static void main(String[] args) {
+        ;
+    }
+}
